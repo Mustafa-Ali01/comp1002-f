f16495a86f6c5d789f1278e2d8ceed24bd672f01
Mustafa Ali <200643639@student.georgianc.on.ca>
2025-10-24 01:24:05 -0400

    Added Assignment-1 Folder, Started work on adding the main code for the frontend

---
+++ assignment-1/process-form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Edit Payment Information - PayDude™</title>
+		<link rel="stylesheet" href="css/styles.css" />
+	</head>
+	<body>
+		<header>
+			<h1>PizzaPalace™</h1>
+		</header>
+		<main>
+			<h2>EEEEEf</h2>
+<?php
+
+	echo('<p>EEEE:</p>
+	<ul>');
+		
+		
+
+
+	echo('</ul>');
+?>
+		</main>
+        <footer>
+            <p><small>©PizzaPalace Inc. All rights reserved</small></p>
+        </footer>
+	</body>
+</html>
