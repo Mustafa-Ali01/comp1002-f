f3972c434de60078accdb89c77c3e1ff9cfbc874
Mustafa-Ali01 <200643639@student.georgianc.on.ca>
2025-11-03 02:00:33 +0000

    Nevermind

--- lab-c/includes/global-nav.php
+++ lab-c/includes/global-nav.php
@@ -3,7 +3,7 @@
 			<nav>
 				<ul>
 					<li><a href="index.php" title="Go to the Home page">Home</a></li>
-					<li><a href="/red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="red.php" title="Learn about red blocks">Red Blocks</a></li>
 					<li><a href="blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
 					<li><a href="yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
 					<li><a href="green.php" title="Learn about green blocks">Green Blocks</a></li> 
